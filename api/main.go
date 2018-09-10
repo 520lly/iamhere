fa3fe08179083b384431f88b968598854bb64cba
jaycee <jaycee_wjq@163.com>
2018-09-10 21:58:30 +0800

    Add https support

--- api/main.go
+++ api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
 	"flag"
 	"gopkg.in/mgo.v2"
 	"log"
@@ -27,8 +28,28 @@ func main() {
 	mux.HandleFunc("/messages/", withCORS(withAPIKey(s.handleMessages)))
 	mux.HandleFunc("/areas/", withCORS(withAPIKey(s.handleAreas)))
 	mux.HandleFunc("/accounts/", withCORS(withAPIKey(s.handleAccounts)))
+
+	cfg := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+	}
+	srv := &http.Server{
+		Addr:         ":443",
+		Handler:      mux,
+		TLSConfig:    cfg,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
+
+	log.Println("Starting https web server on: :443")
+	go srv.ListenAndServeTLS("../assets/certs/server.crt", "../assets/certs/server.key")
 	log.Println("Starting web server on", *addr)
-	//go http.ListenAndServeTLS(":8082", "../assets/certs/server.crt", "../assets/certs/server.key", mux)
 	http.ListenAndServe(":8080", mux)
 	log.Println("Stopping...")
 }
--- assets/gen.sh
+++ assets/gen.sh
@@ -10,7 +10,7 @@ __version__=0.1
 set -e                     #exit when error hanppens
 
 openssl genrsa -out ca.key 2048
-openssl req -x509 -new -nodes -key ca.key -subj "/CN=iamhere.com" -days 5000 -out ca.crt
+openssl req -x509 -new -nodes -key ca.key -subj "/CN=www.historystest.com" -days 5000 -out ca.crt
 openssl genrsa -out server.key 2048
-openssl req -new -key server.key -subj "/CN=localhost" -out server.csr
+openssl req -new -key server.key -subj "/CN=www.historystest.com" -out server.csr
 openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 5000
