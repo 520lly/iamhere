6a233779e64408223b11dd92e39c5543bcb72cc8
wang_j11 <jianqing.wang@preh.cn>
2018-09-11 14:13:33 +0800

    improve auth.sh script

--- assets/auth.sh
+++ assets/auth.sh
@@ -12,4 +12,5 @@ set -e                     #exit when error hanppens
 sudo cp 214987401110045.* /etc/ssl
 sudo openssl genrsa -out server.key 2048
 sudo openssl req -new -key server.key -out server.csr
-sudo openssl x509 -req -in server.csr -CA 214987401110045.pem -CAkey 214987401110045.key -CAcreateserial -out server.crt -days 500
+sudo openssl x509 -req -in server.csr -CA /etc/ssl/214987401110045.pem -CAkey /etc/ssl/214987401110045.key -CAcreateserial -out server.crt -days 500
+sudo mv server.* /etc/ssl/iamhere
