ee8be8b255b089ba2264db8b14fc324e83cff632
jaycee <jaycee_wjq@163.com>
2018-09-12 22:25:47 +0800

    Fix https certificate not secure issue

---
+++ assets/certs/auth.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# encoding: utf-8
+# Name  : auth.sh
+# Descp : used for 
+# Author: jaycee
+# Date  : 11/09/18 11:58:56 +0800
+__version__=0.1
+
+#set -x                     #print every excution log
+set -e                     #exit when error hanppens
+
+sudo cp DV-root.crt 214987401110045.* /etc/ssl
---
+++ assets/certs/readme.txt
@@ -0,0 +1,3 @@
+execute auth.sh
+1. mv DV-root.crt to /etc/ssl
+2. mv 214987401110045.* to /etc/ssl
